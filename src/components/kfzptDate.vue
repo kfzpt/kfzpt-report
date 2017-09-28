da6baefa72e4275a39da264bc2a78d91d2f0b615
xuedongsheng <xuedongsheng@syswin.com>
2017-09-28 14:14:03 +0800

    add footer

--- src/components/kfzptDate.vue
+++ src/components/kfzptDate.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="report_date">
-    <div class="date_info" v-bind:class="{'big_date': date.flag == 1}" v-for="date in dates" track-by="$index">
+  <div class="report_date" id="report_date" @mousedown="onmousedown" @mousemove="onmousemove" @mouseup="onmouseup">
+    <div class="date_info" v-bind:class="{'big_info': date.flag == 1}" v-for="date in dates" track-by="$index">
       <div class="date_event" v-for="event in date.events">
         <li>{{event.title}}</li>
         <li>{{event.time}}</li>
         <img :src="event.img"></img>
         <li>{{event.describe}}</li>
       </div>
-      <div class="date_num" v-bind:class="{'big_date': date.flag == 1}">
+      <div class="date_num" v-bind:class="{'big_num': date.flag == 1}">
         <span>{{date.text}}</span>
       </div>
     </div>
@@ -20,7 +20,47 @@ export default {
   name: 'report_date',
   data () {
     return {
-      dates: appData
+      dates: appData,
+      startMove: 0,
+      startX: 0,
+      startTranslate: 0
+    }
+  },
+  methods: {
+    onmousedown (event) {
+      event = window.event || event
+      this.startMove = 1
+      this.startX = event.clientX
+      var navdrag = document.getElementById('report_date')
+      var translates = navdrag.style.transform
+      if (!translates) {
+        translates = navdrag.style.webkitTransform
+      }
+      if (translates) {
+        this.startTranslate = parseFloat(translates.toString().substring(10).split(',')[0])
+      } else {
+        this.startTranslate = 0
+      }
+    },
+    onmousemove (event) {
+      if (this.startMove === 0) {
+        return
+      }
+      event = window.event || event
+      var distanceX = event.clientX - this.startX
+      event.preventDefault()
+      this.setTitleTransform(distanceX)
+    },
+    onmouseup (event) {
+      this.startMove = 0
+    },
+    setTitleTransform (distance) {
+      var navdrag = document.getElementById('report_date')
+      var touchDelta = distance + this.startTranslate
+      if (touchDelta > 0) {
+        touchDelta = 0
+      }
+      navdrag.style.transform = navdrag.style.webkitTransform = 'translate(' + touchDelta + 'px,0)'
     }
   }
 }
@@ -29,14 +69,15 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .report_date {
-  height: 500px;
+  width: 8000px;
+  height: 580px;
   color: #FFFFFFFF;
 }
 .date_info {
   width: 37px;
   display: inline-block;
   float:left;
-  height: 460px;
+  height: 570px;
   margin-left: 3px;
 }
 .date_event {
@@ -55,8 +96,14 @@ export default {
   text-align: left;
   word-wrap: break-word;
 }
-.big_date {
+.big_info {
+  width: 47px;
+  height: 37px;
+}
+.big_num {
   background-color: red;
+  width: 47px;
+  height: 37px;
 }
 h1 {
   font-weight: normal;
--- src/components/kfzptFooter.vue
+++ src/components/kfzptFooter.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="report_footer">
-    <h1>{{ msg }}</h1>
+  <div class="report_footer" @click="onclick">
   </div>
 </template>
 
@@ -9,7 +8,44 @@ export default {
   name: 'report_footer',
   data () {
     return {
-      msg: '我是底栏'
+      startMove: 0,
+      startTranslate: 0,
+      distance: 0,
+      timer: null
+    }
+  },
+  methods: {
+    onclick (event) {
+      if (this.startMove === 1 && this.timer) {
+        this.startMove = 0
+        clearInterval(this.timer)
+        return
+      }
+      this.startMove = 1
+      var navdrag = document.getElementById('report_date')
+      var translates = navdrag.style.transform
+      if (!translates) {
+        translates = navdrag.style.webkitTransform
+      }
+      if (translates) {
+        this.startTranslate = parseFloat(translates.toString().substring(10).split(',')[0])
+      } else {
+        this.startTranslate = 0
+      }
+      this.distance = 0
+      var that = this
+      this.timer = setInterval(function () {
+        that.distance -= 5
+        that.setTitleTransform(that.distance)
+      }, 100)
+    },
+    setTitleTransform (distance) {
+      var navdrag = document.getElementById('report_date')
+      var touchDelta = distance + this.startTranslate
+      if (touchDelta > 0) {
+        touchDelta = 0
+      }
+      navdrag.style.transform = navdrag.style.webkitTransform = 'translate(' + touchDelta + 'px,0)'
     }
   }
 }
@@ -20,9 +56,8 @@ export default {
 .report_footer {
   height: 40px;
   color: #FFFFFFFF;
+  background-image: url(../assets/play.png);
+  background-repeat: no-repeat;
+  background-position: center;
 }
-h1 {
-  font-weight: normal;
-}
-
 </style>
